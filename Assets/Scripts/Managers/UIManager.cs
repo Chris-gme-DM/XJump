a964d2fbe202c02e11d1989e1e07eecf2f873a82
Christof Kloninger <kloningerchristof@gmail.com>
2025-07-17 15:11:49 +0200

    Auto stash before merge of "main" and "UIfeature"

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,7 +1,32 @@
 using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEngine.Rendering.DebugUI;
 
 public class UIManager : MonoBehaviour
 {
+    /// <summary>
+    /// References for Canvas Elements, Set in Inspector and copy Canvas into GameScene
+    /// References: OptionPanel, TitlePanel, ScorePanel
+    /// </summary>
+    [Header("Canvas References")]
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private Panel titlePanel;
+    [SerializeField] private Panel optionsPanel;
+    [SerializeField] private Panel scorePanel;
+
+    [Header("OptionsPanels")]
+    [SerializeField] private Panel musicSettingsPanel;
+    [SerializeField] private Panel soundSettingsPanel;
+
+    // Internal References
+    private int score;
+    private int musicVloume;
+    private int soundVloume;
+
+    // Public Getter for SoundSettings
+    [HideInInspector] public int MusicVolume => musicVloume;
+    [HideInInspector] public int SoundVolume => soundVloume;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +36,6 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
