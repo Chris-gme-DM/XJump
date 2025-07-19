f7ac345a7710ad86e9b578c7086eedb729e305ed
BTLV444\Osman <2024.gme.osmansen@gmail.com>
2025-07-19 11:26:41 +0200

    Implements level progression
    Adds level progression based on player height.
    Updates jump pad colors to reflect level.

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-    [SerializeField] private int stageIndex;
-    private int currentStageIndex = 0;
+    [SerializeField] private int currentStageIndex;
+    [SerializeField] private int stageIndexHelper;
     [SerializeField] private GameObject JumpPadsParent;
     [SerializeField] private float[] stageHeights;
 
@@ -18,4 +18,54 @@ public class LevelManager : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < stageHeights.Length; i++)
+        {
+            if (player.transform.position.y >= stageHeights[i] && currentStageIndex != i && currentStageIndex !< i)
+            {
+                currentStageIndex = i;
+                Debug.Log("Stage " + (i + 1) + " reached! currentStageIndex: " + currentStageIndex);
+                // You can add additional logic here for when a new stage is reached
+            }
+        }
+
+        if (stageIndexHelper < currentStageIndex)
+        {
+            stageIndexHelper = currentStageIndex;
+            foreach(Transform jumpPad in JumpPadsParent.transform)
+            {
+                SpriteRenderer sr = jumpPad.gameObject.GetComponent<SpriteRenderer>();
+                switch (currentStageIndex)
+                {
+                    case 0:
+                        sr.color = Color.white; // Default color for stage 1
+                        break;
+                    case 1:
+                        sr.color = Color.green; // Color for stage 2
+                        break;
+                    case 2:
+                        sr.color = Color.blue; // Color for stage 3
+                        break;
+                    case 3:
+                        sr.color = Color.red; // Color for stage 4
+                        break;
+                    case 4:
+                        sr.color = Color.yellow; // Color for stage 4
+                        break;
+                    default:
+                        sr.color = Color.white; // Fallback color
+                        break;
+                }
+            }
+        }
+
+    }
+
+    public int GetCurrentStageIndex()
+    {
+        return currentStageIndex;
+    }
+
 }
+
