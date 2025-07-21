89937e5b6db51284a4eaa2b55d029303718fcad1
BTLV444\Osman <2024.gme.osmansen@gmail.com>
2025-07-21 15:23:35 +0200

    add jump to player edit collisions on jump pads

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -1,4 +1,8 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
 
 public class JumpPad : MonoBehaviour
 {
@@ -9,5 +13,21 @@ public class JumpPad : MonoBehaviour
         newX = Mathf.Clamp(newX, minX, maxX);
         transform.position = new Vector3(newX, transform.position.y + distance, transform.position.z); 
 
+    }
+
+    public float jumpForce = 10f; // Force applied when the player jumps on the pad
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.y <= 0f)
+
+        {
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+              Vector2 velocity = rb.linearVelocity;
+                velocity.y = jumpForce; // Set the vertical velocity to the jump force
+                rb.linearVelocity = velocity; 
+            }
+        }
     }
 }
